f596e23cee8b1514b417278585bc97d0aad0be82
carlos <carloshamata9@gmail.com>
2023-05-19 02:40:57 +0200

    Use magic to identify llama models

--- llama.cpp
+++ llama.cpp
@@ -10,8 +10,12 @@
 
 extern "C" {
 const LM::Implementation *get_justlm_implementation() {
-    static LM::Implementation fres{true};
+    static LM::Implementation fres{false};
     return &fres;
+}
+
+bool magic_match(uint32_t magic) {
+    return magic == 0x67676a74;
 }
 
 LM::Inference *construct(const std::string &weights_path, std::ifstream& f, const LM::Inference::Params &p) {
